4c397f2dd6b0b52b94dc9978fd7bcd4410a86704
Steve Hayman <steve@hayman.net>
2015-09-03 07:15:31 -0700

    simple commit

--- UselessApp/ViewController.swift
+++ UselessApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("whatever")
+        print("whatever you want")
     }
 
     override func didReceiveMemoryWarning() {
