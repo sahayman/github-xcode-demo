0ce0e8cc78fc74aaf43de5da2961f26d00987b9e
Steve Hayman <steve@hayman.net>
2015-09-03 07:09:16 -0700

    Initial Commit

---
+++ UselessApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UselessApp
+//
+//  Created by Steve Hayman on 2015-09-03.
+//  Copyright © 2015 Hayman Networking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
